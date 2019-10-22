523dee244baeb518f85f4197bdd2b8719fce9958
zscaiosi <zscaio.si@gmail.com>
2019-10-22 21:05:31 +0000

    feature: Fixing BaseStartup and finishing whole flow;

--- Startup/BaseStartup.cs
+++ Startup/BaseStartup.cs
@@ -20,17 +20,18 @@ namespace Startup
     public abstract class BaseStartup
     {
         public IConfiguration _configs {get;}
-        private readonly List<CorsPolicy> _corsPolicies;
+        private readonly CorsPolicy _corsPolicy;
         private readonly EStartupType _type;
         private readonly List<EConfigs> _eConfigs;
 
-        public BaseStartup(IConfiguration configuration, EStartupType type, List<EConfigs> eConfigs, List<CorsPolicy> corsPolicies) {
+        public BaseStartup(IConfiguration configuration, EStartupType type, List<EConfigs> eConfigs, CorsPolicy corsPolicy) {
             _configs = configuration;
             _type = type;
             _eConfigs = eConfigs;
-            _corsPolicies = corsPolicies;
+            _corsPolicy = corsPolicy;
         }
-
+        /// <summary>Adds JWT Authentication</summary>
+        /// <param name="services">The main IServiceCollection's reference</param>
         private void AddBearerAuthentication(ref IServiceCollection services) {
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(bearerOpt => {
@@ -58,51 +59,69 @@ namespace Startup
                 {
                     OnMessageReceived = context =>
                     {
-                        context.HttpContext
+                        Console.WriteLine("Client Address Family:");
+                        Console.WriteLine(context.HttpContext.Connection.RemoteIpAddress.AddressFamily);
+                        Console.WriteLine("Client Address IsIPv4MappedToIPv6:");
+                        Console.WriteLine(context.HttpContext.Connection.RemoteIpAddress.IsIPv4MappedToIPv6);
+                        Console.WriteLine("Client Port:");
+                        Console.WriteLine(context.HttpContext.Connection.RemotePort);
+
                         return Task.CompletedTask;
                     },
                     OnAuthenticationFailed = context =>
                     {
-                        Console.WriteLine("Token inválido..:. " + context.Exception.Message);
+                        Console.WriteLine("Token inválido... " + context.Exception.Message);
+                        // If has redirect url, then applies else sets status code
+                        var redirect = _configs["UnauthorizedRedirect"];
+
+                        if (!string.IsNullOrEmpty(redirect))
+                            context.Response.Redirect(redirect);
+                        else
+                            context.Response.StatusCode = 401;
+
                         return Task.CompletedTask;
                     },
                     OnTokenValidated = context =>
                     {
-                        Console.WriteLine("Token válido...: " + context.SecurityToken);
+                        Console.WriteLine("Token válido... " + context.SecurityToken);
                         return Task.CompletedTask;
                     }
                 };
             });
         }
-
-        protected virtual void BeforeConfigureServices(IServiceCollection services) {
+        /// <summary>Called at top of ConfigureServices</summary>
+        /// <param name="services">The main IServiceCollection's reference</param>
+        protected virtual void BeforeConfigureServices(ref IServiceCollection services) {
             
         }
-
-        public virtual void BeforeConfigureApp(IApplicationBuilder app, IWebHostEnvironment env) {
+        /// <summary>Called at top of Configure</summary>
+        /// <param name="services">The main IServiceCollection's reference</param>
+        public virtual void BeforeConfigureApp(ref IApplicationBuilder app, ref IWebHostEnvironment env) {
             
         }
         public void ConfigureServices(IServiceCollection services) {
-            BeforeConfigureServices(services);
+            BeforeConfigureServices(ref services);
             // CORS Policy
             services.AddCors(options =>
             {
-                foreach (var p in _corsPolicies) {
-                    options.AddPolicy(p.Name,
-                    builder =>
-                    {
-                        builder
-                        .WithHeaders(p.Headers)
-                        .WithMethods(p.Methods)
-                        .WithOrigins(p.Origins);
-                    });
-                }
+                options.AddPolicy(_corsPolicy.Name,
+                builder =>
+                {
+                    builder
+                    .WithHeaders(_corsPolicy.Headers)
+                    .WithMethods(_corsPolicy.Methods)
+                    .WithOrigins(_corsPolicy.Origins);
+                });
             });
             // JWT Bearer Token
             AddBearerAuthentication(ref services);
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            BeforeConfigureApp(app, env);
+            BeforeConfigureApp(ref app, ref env);
+
+            app.UseCors(_corsPolicy.Name);
+            app.UseAuthorization();
+            app.UseMvc();
         }
     }
 }
