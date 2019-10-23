b35d6aa9b69aa671b087e66045c5a69a55854fab
zscaiosi <zscaio.si@gmail.com>
2019-10-23 12:38:08 +0000

    feature: TDD;

---
+++ StartupTest/BaseStartupTDD.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StartupTest
+{
+    public class BaseStartupTDD
+    {
+        [Fact]
+        public void IsInstanceOf()
+        {
+
+        }
+    }
+}
